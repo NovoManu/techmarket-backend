2e0465181bca6b2890ca6182cc30a627921c2695
NovoManu <operamike@mail.ru>
2018-11-27 22:22:23 +0200

    Auth: added login/signup. Response with header

--- common/models/client.js
+++ common/models/client.js
@@ -1,36 +1,52 @@
 'use strict';
+const jwtDecode = require('jwt-decode');
+// Note: we are adding the same password for all user because we have auth0
+const password = 'techMarketuserLogin45372';
+
+function loginClient(Model, {email}, callback) {
+  Model.login({
+    email,
+    password,
+  }, 'client', (err, res) => {
+    callback(null, res);
+  });
+}
 
 module.exports = function(Client) {
   /**
    * Login an existing user or create a new one
-   * @param {string} name (not required)
-   * @param {string} email (required)
-   * @param {string} phone (not required)
+   * @param {string} body (not required)
    * @param {Function(Error, object)} callback
    */
 
-  Client.loginOrCreate = function(name = '', email, phone = '', callback) {
+  Client.loginOrCreate = (
+    req, name = '', email = '', phone = '', callback
+  ) => {
+    const token = req.query['access_token'];
+    const decodedToken = jwtDecode(token);
+    const sub = decodedToken.sub;
     Client.find({
-      where: {email},
+      where: {sub},
     }, (err, res) => {
-      if (err) {
-        callback(err);
-      }
+      if (err) callback(err);
       if (res && res.length) {
-        callback(null, res[0]);
+        loginClient(Client, {email: res[0]['email']}, callback);
       } else {
-        const refNumber = Math.floor(Math.random() * (9999 - 1000) + 1000);
-        Client.create({
+        const randomNumber = Math.floor(Math.random() * (9999 - 1000) + 1000);
+        const refNumber = `${email}-${randomNumber}`;
+        const params = {
+          sub,
           name,
           email,
-          password: phone,
+          password,
           phone,
           address: '',
-          'ref_link': `${email}:${refNumber}`,
+          'ref_link': refNumber,
           'refered_by': '',
-        }, (err, result) => {
-          // if (err) callback(err);
-          if (res) callback(null, result);
+        };
+        Client.create(params, (err, result) => {
+          if (err) callback(err);
+          if (res) loginClient(Client, {email: result.email}, callback);
         });
       }
     });
