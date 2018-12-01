7e69e25c45e0b2b1a9a2ec59e1086004223fa3f0
NovoManu <operamike@mail.ru>
2018-12-01 15:26:07 +0200

    Changed path to api

--- server/config.local.js
+++ server/config.local.js
@@ -4,7 +4,7 @@ const p = require('../package.json');
 const version = p.version.split('.').shift();
 
 module.exports = {
-  restApiRoot: (version > 0 ? '/v' + version : ''),
+  restApiRoot: (version > 0 ? '/api/v' + version : ''),
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000,
 };
