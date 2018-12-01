27dc5c9bc37d5c83f548c779cb8079c84ab0ee29
NovoManu <operamike@mail.ru>
2018-12-01 15:14:47 +0200

    Changed path to api

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    var baseUrl = app.get('url').replace(/\/$/, '/api');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
